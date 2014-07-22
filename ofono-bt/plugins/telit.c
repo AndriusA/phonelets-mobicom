ce46093234cd138712b5ad7fa4efa6b0a0fc2847
Andrius Aucinas <andrius.aucinas@gmail.com>
2014-07-22 15:26:31 +0100

    added the timeout/gpio changes to bt version as well

--- ofono-bt/plugins/telit.c
+++ ofono-bt/plugins/telit.c
@@ -367,6 +367,10 @@ static int telit_enable(struct ofono_modem *modem)
 	g_at_chat_send(data->chat, "ATE0 +CMEE=1", none_prefix,
 				NULL, NULL, NULL);
 
+	// Set GPIO7 pin as BUZZER OUTPUT
+	g_at_chat_send(data->chat, "AT#GPIO=7,0,2", none_prefix,
+				NULL, NULL, NULL);
+	
 	/*
 	 * Disable sim state notification so that we sure get a notification
 	 * when we enable it again later and don't have to query it.
